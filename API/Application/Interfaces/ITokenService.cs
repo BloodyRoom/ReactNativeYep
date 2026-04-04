d56849ceab0fec750c931af367088e7b11e12d26
BloodyRoom <sabadashkamaksym@gmail.com>
2026-04-04 23:05:09 +0300

    init

---
+++ API/Application/Interfaces/ITokenService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces;
+
+public interface ITokenService
+{
+    Task<string> CreateTokenAsync(UserEntity user);
+    Task<string> GenerateRefreshTokenAsync();
+    Task RevokeRefreshTokenAsync(string token);
+}
